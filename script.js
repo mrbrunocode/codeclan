7a7225ec803a9fee2cea5def4e26c7e0975c4c91
Bruno <mrbruno@gmail.com>
2018-10-01 20:30:19 +0100

    half way through exercise

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 /* Exercise 1: Wish list */
 function addToList(item){
-  $('#items').append('<li>' + item + '</li>')
+//  $('#items').append('<li>' + item + '</li>');
+  $('#items').append('<li>' + item + "<span class='label pending'>" + 'Pending' + '</span>' + '</li>');
 }
+
+
+$(document).on('click', '#add-to-list', function() {
+addToList($('#item').val());
+$('#item').val('');
+$('#item').focus();
+
+
+});
